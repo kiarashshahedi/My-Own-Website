1004ef3356cfa000edcc39a982047528ba7f0117
Kiarash <shahedikiarash@gmail.com>
2024-09-04 20:18:19 +0330

    fixing api JWT

--- core/core/urls.py
+++ core/core/urls.py
@@ -1,5 +1,3 @@
-from django.contrib import admin
-from django.urls import path, include
 
 # import static and setting for loading static files
 from django.conf import settings
@@ -8,33 +6,30 @@ from django.conf.urls.static import static
 # for api documentation 
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register(r'products', ProductsViewSet)
+router.register(r'categories', CategoryViewSet)
+router.register(r'reviews', ReviewViewSet)
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="TakKharid API",
+      title="TakKharid API Documentation",
       default_version='v1',
-      description="API documentation for your TakKharid WebSite",
+      description="API documentation",
+      contact=openapi.Contact(email="OGODE..."),
    ),
    public=True,
-   permission_classes=(permissions.AllowAny,),
 )
-
-
-
 urlpatterns = [
    path('admin/', admin.site.urls),
-   # normal usrls
-   # path('accounts/', include('accounts.urls')),
-
    # API urls
    path('accounts/', include('accounts.urls')),
    path('products/', include('products.urls')),
-
-   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
